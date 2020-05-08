e6f680e2f86b3d5656d9629aa21560fce27366e9
Morgan Collado <morgan.collado@gmail.com>
2020-05-08 19:37:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,18 +15,17 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["I", "am", "really", "learning", "arrays!"]
   array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-   array[2]
+   array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
